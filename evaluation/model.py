8fb0e0e8c8a5a0efcd3cdeff8c2d8643adb2dcc0
Chus Antoñanzas <jemanac1998@gmail.com>
2021-05-06 17:41:29 +0200

    Pipeline struct.

--- evaluation/model.py
+++ evaluation/model.py
@@ -11,6 +11,8 @@ from explanation.images import torch_pixel_attributions
 from syntheticData.image import gen_image_data
 from syntheticData.tabular import gen_tabular_data
 
+# todo merge functions eval_torch_tab, eval_torch_image and their dependencies
+
 
 def _init_sk_model(classname, modelParams):
     """ Initialise sklearn model from string given the dict of params 'modelParams'. For it to work the model
@@ -25,8 +27,7 @@ def _gen_split_data(dataType, nSamples, nFeatures, randomState, expType, dataSpl
 
     if dataType == 'image':
         # todo further parameters to gen_image_data
-        # in the case of images, the binary g.t. masks are the explanations
-        X, y, _ = gen_image_data(nSamples=nSamples, randomState=randomState)
+        X, y, gtExp, _ = gen_image_data(nSamples=nSamples, randomState=randomState)
     elif dataType == 'tab':
         X, y, gtExp, featureNames = gen_tabular_data(nSamples, nFeatures, randomState, expType)
     else:
@@ -45,19 +46,17 @@ def _gen_split_data(dataType, nSamples, nFeatures, randomState, expType, dataSpl
     trainSize = round(len(y) * dataSplit[0])
     valSize = round(len(y) * dataSplit[1])
 
+    XTrain, XTest, yTrain, yTest, gtExpTrain, gtExpTest = train_test_split(X, y, gtExp, train_size=trainSize,
+                                                                           random_state=randomState)
+    XTrain, XVal, yTrain, yVal, gtExpTrain, gtExpVal = train_test_split(XTrain, yTrain, gtExpTrain, test_size=valSize,
+                                                                        random_state=randomState)
     if dataType == 'image':
-        XTrain, XTest, yTrain, yTest = train_test_split(X, y, train_size=trainSize, random_state=randomState)
-        XTrain, XVal, yTrain, yVal = train_test_split(XTrain, yTrain, test_size=valSize, random_state=randomState)
-        return XTrain, XVal, XTest, yTrain, yVal, yTest
-    elif dataType == 'tab':
-        XTrain, XTest, yTrain, yTest, gtExpTrain, gtExpTest = train_test_split(X, y, gtExp, train_size=trainSize,
-                                                                               random_state=randomState)
-        XTrain, XVal, yTrain, yVal, gtExpTrain, gtExpVal = train_test_split(XTrain, yTrain, gtExpTrain, test_size=valSize,
-                                                                            random_state=randomState)
+        return XTrain, XVal, XTest, yTrain, yVal, yTest, gtExpTrain, gtExpVal, gtExpTest
+    else:
         return XTrain, XVal, XTest, yTrain, yVal, yTest, gtExpTrain, gtExpVal, gtExpTest, featureNames
 
 
-# todo should feature importances be normalised for the linear method?
+# todo normalise explanations for the linear method
 def eval_sk_tabular(model, nSamples, nFeatures, dataSplit, expMethod, expType, randomState=888):
     """ Trains a sklearn model with synthetic data. Then, generates explanations and evaluates them with
     the available ground truths for the generated train, validation and test sets.
@@ -73,7 +72,8 @@ def eval_sk_tabular(model, nSamples, nFeatures, dataSplit, expMethod, expType, r
     :param randomState: (int) random seed.
     """
     XTrain, XVal, XTest, yTrain, yVal, yTest, \
-        gtExpTrain, gtExpVal, gtExpTest = _gen_split_data('tab', nSamples, nFeatures, randomState, expType, dataSplit)
+    gtExpTrain, gtExpVal, gtExpTest, _ = _gen_split_data('tab', nSamples, randomState, expType,
+                                                         dataSplit, nFeatures)
 
     model.fit(XTrain, yTrain)
 
@@ -102,20 +102,21 @@ def eval_torch_tab(model, trainFunction, nSamples, nFeatures, dataSplit, expMeth
     :param randomState: (int) random seed.
     """
     XTrain, XVal, XTest, yTrain, yVal, yTest, \
-        gtExpTrain, gtExpVal, gtExpTest = _gen_split_data('tab', nSamples, nFeatures, randomState, expType, dataSplit)
-
+        gtExpTrain, gtExpVal, gtExpTest, fNames = _gen_split_data(dataType='tab',
+                                                                  nSamples=nSamples,
+                                                                  expType=expType,
+                                                                  dataSplit=dataSplit,
+                                                                  nFeatures=nFeatures,
+                                                                  randomState=randomState)
     XTrain = torch.FloatTensor(XTrain)
     XVal = torch.FloatTensor(XVal)
     XTest = torch.FloatTensor(XTest)
-    gtExpTrain = torch.FloatTensor(gtExpTrain)
-    gtExpVal = torch.FloatTensor(gtExpVal)
-    gtExpTest = torch.FloatTensor(gtExpTest)
 
     yTrain = torch.LongTensor(yTrain)
     yVal = torch.LongTensor(yVal)
     yTest = torch.LongTensor(yTest)
 
-    model = trainFunction(model, XTrain, yTrain)
+    model, avgLoss, avgAcc = trainFunction(model, XTrain, yTrain)
 
     expTrain = torch_tab_attributions(model, XTrain, yTrain, method=expMethod)
     expVal = torch_tab_attributions(model, XVal, yVal, method=expMethod)
@@ -124,7 +125,7 @@ def eval_torch_tab(model, trainFunction, nSamples, nFeatures, dataSplit, expMeth
     return
 
 
-def eval_torch_image(model, trainFunction, nSamples, nFeatures, dataSplit, expMethod, expType, randomState=888):
+def eval_torch_image(model, trainFunction, nSamples, dataSplit, expMethod, randomState=888):
     """
     Trains a PyTorch model with synthetic image data. Then, generates explanations and evaluates them with
     the available ground truths for the train, validation and test sets.
@@ -134,28 +135,34 @@ def eval_torch_image(model, trainFunction, nSamples, nFeatures, dataSplit, expMe
                           model), 'X' (the training data as a ndarray)  and 'y' (g.t. labels as a ndarray) and returns
                           the trained model.
     :param nSamples: (int) number of samples to create.
-    :param nFeatures: (int) number of features the artificial data will have.
     :param dataSplit: (array-like) list like object with 3 floats, each indicating the proportion of data samples
                                    to use as train, validation and test, respectively. Must sum 1. i.e. (0.6, 0.2, 0.2)
-    :param expMethod: (string) explanation method to generate the explanations with. Available ['shap', 'lime']
-    :param expType: (string) type of ground truth explanations. Available:
-                             'fi' (feature importance vect.), 'rule' (DecisionRule objects)
+    # todo update supported methods
+    :param expMethod: (string) explanation method to generate the explanations with. Available ['kernelShap', 'lime']
     :param randomState: (int) random seed.
     """
-    XTrain, XVal, XTest, yTrain, yVal, yTest = _gen_split_data('image', nSamples, nFeatures, randomState, expType,
-                                                               dataSplit)
+    # todo parametrize image configurations
+    expType, nFeatures = None, None
+    XTrain, XVal, XTest, yTrain, yVal, yTest, \
+    gtExpTrain, gtExpVal, gtExpTest = _gen_split_data('image', nSamples, nFeatures, randomState, expType, dataSplit)
 
     XTrain = torch.FloatTensor(XTrain).reshape(-1, 3, 32, 32)
     XVal = torch.FloatTensor(XVal).reshape(-1, 3, 32, 32)
     XTest = torch.FloatTensor(XTest).reshape(-1, 3, 32, 32)
 
-    yTrain = torch.LongTensor(yTrain.astype(int)).reshape(-1, 1, 32, 32)
-    yVal = torch.LongTensor(yVal.astype(int)).reshape(-1, 1, 32, 32)
-    yTest = torch.LongTensor(yTest.astype(int)).reshape(-1, 1, 32, 32)
+    gtExpTrain = torch.FloatTensor(gtExpTrain).reshape(-1, 1, 32, 32)
+    gtExpVal = torch.FloatTensor(gtExpVal).reshape(-1, 1, 32, 32)
+    gtExpTest = torch.FloatTensor(gtExpTest).reshape(-1, 1, 32, 32)
 
-    model = trainFunction(model, XTrain, yTrain)
+    yTrain = torch.LongTensor(yTrain)
+    yVal = torch.LongTensor(yVal)
+    yTest = torch.LongTensor(yTest)
 
-    expTrain = torch_pixel_attributions(model, XTrain, method=expMethod)
+    model, avgLoss, avgAcc = trainFunction(model, XTrain, yTrain)
+
+    print(f'Trained model with avg. training loss of {avgLoss} and avg. training accuracy of {avgAcc}.')
+
+    expTrain = torch_pixel_attributions(model, XTrain, yTrain, method=expMethod)
     # expVal = gen_explanations(model, XVal, method=expMethod)
     # expTest = gen_explanations(model, XTest, method=expMethod)
 
@@ -170,14 +177,13 @@ if __name__ == '__main__':
     import torch.optim as optim
     import torch.nn.functional as F
 
-    nFeat = 5
 
     class TabNet(nn.Module):
         """ Basic FC NN """
 
-        def __init__(self):
+        def __init__(self, nFeatures):
             super(TabNet, self).__init__()
-            self.fc1 = nn.Linear(nFeat, 25)  # 5*5 from image dimension
+            self.fc1 = nn.Linear(nFeatures, 25)  # 5*5 from image dimension
             self.fc2 = nn.Linear(25, 15)
             self.fc3 = nn.Linear(15, 2)
 
@@ -187,29 +193,10 @@ if __name__ == '__main__':
             x = self.fc3(x)
             return x
 
-    # sample training function for class Net
-    def train_tabnet(model, X, y):
-        """ X: FloatTensor, y: LongTensor """
-        batchSize = 20
-        nEpochs = 10
-        criterion = nn.CrossEntropyLoss()
-        optimizer = optim.SGD(model.parameters(), lr=1e-3)
-        for epoch in range(nEpochs):
-            for batch in range(int(len(X) / batchSize)):
-                XBatch = X[batch:batch + batchSize]
-                yBatch = y[batch:batch + batchSize]
-                out = model(XBatch)
-                loss = criterion(out, yBatch)
-
-                model.zero_grad()
-                loss.backward()
-                optimizer.step()
-        return model
-
 
     class ImNet(nn.Module):
         # https://medium.com/analytics-vidhya/creating-a-very-simple-u-net-model-with-pytorch-for-semantic-segmentation-of-satellite-images-223aa216e705
-        def __init__(self, in_channels, out_channels):
+        def __init__(self, in_channels, out_channels, imH, imW):
             super().__init__()
 
             self.conv1 = self.contract_block(in_channels, 32, 7, 3)
@@ -220,18 +207,22 @@ if __name__ == '__main__':
             self.upconv2 = self.expand_block(64 * 2, 32, 3, 1)
             self.upconv1 = self.expand_block(32 * 2, out_channels, 3, 1)
 
+            self.linear = nn.Linear(out_channels * imH * imW, 2)
+
         def forward(self, x):
-            # downsampling part
+            # downsampling
             conv1 = self.conv1(x)
             conv2 = self.conv2(conv1)
             conv3 = self.conv3(conv2)
 
+            # upsampling
             upconv3 = self.upconv3(conv3)
-
             upconv2 = self.upconv2(torch.cat([upconv3, conv2], 1))
             upconv1 = self.upconv1(torch.cat([upconv2, conv1], 1))
 
-            return upconv1
+            linOut = self.linear(upconv1.reshape(upconv1.shape[0], -1))
+
+            return linOut
 
         @staticmethod
         def contract_block(in_channels, out_channels, kernel_size, padding):
@@ -260,29 +251,65 @@ if __name__ == '__main__':
                                    )
             return expand
 
-    # sample training function for class Net
-    def train_imnet(model, X, y):
-        # todo y FloatTensor
-        """ X: FloatTensor, y: FloatTensor """
+
+    from sklearn.metrics import accuracy_score
+
+    # sample training function for binary classification
+    def train_net(model, X, y, randomState=888):
+        """ X: FloatTensor, y: LongTensor """
+        torch.manual_seed(randomState)
         batchSize = 20
         nEpochs = 10
-        criterion = nn.BCELoss()
-        optimizer = optim.SGD(model.parameters(), lr=1e-3)
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(model.parameters(), lr=1e-3)
+        avgLoss, avgAcc = None, None
         for epoch in range(nEpochs):
+            model.train()
             for batch in range(int(len(X) / batchSize)):
                 XBatch = X[batch:batch + batchSize]
                 yBatch = y[batch:batch + batchSize]
-                out = F.softmax(model(XBatch), dim=1)
+                model.zero_grad()
+                out = model(XBatch)
                 loss = criterion(out, yBatch)
 
-                model.zero_grad()
                 loss.backward()
                 optimizer.step()
-        return model
+            model.eval()
+            avgLoss = 0
+            avgAcc = 0
+            for batch in range(int(len(X) / batchSize)):
+                XBatch = X[batch:batch + batchSize]
+                yBatch = y[batch:batch + batchSize]
+                out = model(XBatch)
+                loss = criterion(out, yBatch)
+                avgLoss += loss.item()
+                preds = F.softmax(out, dim=-1).argmax(dim=1)
+                acc = accuracy_score(yBatch, preds.detach().numpy())
+                avgAcc += acc
+            avgLoss /= int(len(X) / batchSize)
+            avgAcc /= int(len(X) / batchSize)
 
+        return model, avgLoss, avgAcc
 
-    eval_torch_image(ImNet(3, 1), train_tabnet, 50, nFeat, (0.8, 0.1, 0.1), 'shap', 'rule')
+    # image Umodel with pixel importance explanations
+    nImages = 100
+    torch.manual_seed(888)
+    imModel = ImNet(3, 1, 32, 32)
+    print('Training image model...')
+    eval_torch_image(model=imModel,
+                     trainFunction=train_net,
+                     nSamples=nImages,
+                     dataSplit=(0.8, 0.1, 0.1),
+                     expMethod='lime')
 
-    # eval_torch_tab(Net(), train_tabnet, 500, nFeat, (0.8, 0.1, 0.1), 'shap', 'rule')
-
-    # eval_sk_tabular(DecisionTreeClassifier(), 1000, 5, (0.7, 0.15, 0.15), 'lime', 'rule')
+    # tabular model with feature importance explanations
+    nFeat = 5
+    torch.manual_seed(888)
+    tabModel = TabNet(nFeat)
+    eval_torch_tab(model=tabModel,
+                   trainFunction=train_net,
+                   nSamples=500,
+                   nFeatures=nFeat,
+                   dataSplit=(0.8, 0.1, 0.1),
+                   expType='rule',
+                   expMethod='shap')
--- explanation/images.py
+++ explanation/images.py
@@ -1,10 +1,100 @@
 from math import sqrt
 
+import numpy as np
 import torch
 from captum._utils.models import SkLearnLinearModel
-from captum.attr._core.lime import get_exp_kernel_similarity_function, LimeBase
+from captum.attr import KernelShap, IntegratedGradients, GradientShap, Occlusion, GuidedBackprop, DeepLift, \
+    GuidedGradCam
+from captum.attr._core.lime import get_exp_kernel_similarity_function, LimeBase, Lime
 
 from explanation.featureImportance import _lime_perturb_func, _lime_identity
+
+
+def get_image_explainer(model, layer=None, method='guidedGradCAM', model_name=None, **kwargs):
+    """ Gets a captum explainer based on the provided PyTorch model.
+    :param model: (callable) PyTorch classification model
+    :param layer: (object) Model layer to use guidedGradCAM on. Predefined layer numbers are automatically defined if model_name
+                  is in ['resnet18', 'resnet50', 'alexnet', 'vgg', 'squeezenet', 'densenet', 'inception'].
+    :param method: (str) explanation method. Available: ['lime', 'integratedGradient', 'gradientShap', 'kernelShap',
+    'occlusion', 'guidedBackProp', 'deepLift', 'guidedGradCAM']
+    :param model_name: (str) Model architecture. Only used if layer=None and method='guidedGradCAM'.
+                       Options: ['resnet18', 'resnet50', 'alexnet', 'vgg', 'squeezenet', 'densenet', 'inception']
+    :param kwargs: extra keyword arguments. Will be passed to the explainer's __init__ method (for those that support
+                   extra arguments).
+    :return: explainer object.
+    """
+    if method == 'lime':
+        explainer = Lime(model, **kwargs)
+    elif method == 'integratedGradient':
+        explainer = IntegratedGradients(model, **kwargs)
+    elif method == 'gradientShap':
+        explainer = GradientShap(model, **kwargs)
+    elif method == 'kernelShap':
+        explainer = KernelShap(model)
+    elif method == 'occlusion':
+        explainer = Occlusion(model)
+    elif method == 'guidedBackProp':
+        explainer = GuidedBackprop(model)
+    elif method == 'deepLift':
+        explainer = DeepLift(model, **kwargs)
+    elif method == 'guidedGradCAM':
+        if layer is None:
+            if model_name == 'resnet18':
+                layer = model.layer4
+            elif model_name == 'resnet50':
+                layer = model.layer4
+            elif model_name == 'alexnet':
+                layer = model.features[10]
+            elif model_name == 'vgg':
+                layer = model.features[25]
+            elif model_name == 'squeezenet':
+                layer = model.features[12]
+            elif model_name == 'densenet':
+                layer = model.features.denseblock4.denselayer16
+            elif model_name == 'inception':
+                layer = model.Mixed_7c
+        explainer = GuidedGradCam(model, layer)
+    else:
+        explainers = ['lime', 'integratedGradient', 'gradientShap', 'kernelShap', 'occlusion', 'guidedBackProp',
+                      'deepLift', 'guidedGradCAM']
+        raise ValueError(f'Explainer not supported. Use {explainers}')
+
+    return explainer
+
+
+def get_image_attributions(explainer, inputImg, target=None, method=None, randomSeed=888):
+    """ Returns attribution scores for the specified image and explainer. """
+    if method == 'integratedGradient':
+        attributions = explainer.attribute(inputImg, target=target, n_steps=200)
+    elif method == 'gradientShap':
+        torch.manual_seed(randomSeed)
+        np.random.seed(randomSeed)
+        # Defining baseline distribution of images
+        # rand_img_dist = torch.cat([inputImg * 0, inputImg * 1])
+
+        attributions = explainer.attribute(inputImg,
+                                           n_samples=50,
+                                           stdevs=0.0001,
+                                           baselines=0,
+                                           target=target)
+    elif method == 'kernelShap':
+        attributions = explainer.attribute(inputImg,
+                                           baselines=0,
+                                           target=target)
+    elif method == 'occlusion':
+        attributions = explainer.attribute(inputImg,
+                                           strides=(3, 8, 8),
+                                           target=target,
+                                           sliding_window_shapes=(3, 15, 15),
+                                           baselines=0)
+    elif method in {'lime', 'kernelShap', 'guidedBackProp', 'deepLift', 'guidedGradCAM'}:
+        attributions = explainer.attribute(inputImg, target)
+    else:
+        explainers = ['lime', 'integratedGradient', 'gradientShap', 'kernelShap', 'occlusion', 'guidedBackProp',
+                      'deepLift', 'guidedGradCAM']
+        raise ValueError(f'Explainer not supported. Use {explainers}')
+
+    return attributions
 
 
 def lime_image_attributions(model, data, labelsToExplain):
@@ -17,18 +107,35 @@ def lime_image_attributions(model, data, labelsToExplain):
                         perturb_interpretable_space=False,
                         from_interp_rep_transform=None,
                         to_interp_rep_transform=_lime_identity)
-    if len(data.shape) != 2:
-        raise ValueError('Data should be 2D (nObs x nFeatures).')
+    if len(data.shape) != 4:
+        raise ValueError('Data should be 4D (nObs x channels x height x width).')
 
     attr = torch.FloatTensor([])
     for obs, label in zip(data, labelsToExplain):
-        attr = torch.cat((attr, limeAttr.attribute(obs.reshape(1, -1), target=label)))
+        attr = torch.cat((attr, limeAttr.attribute(obs.reshape(1, 3, 32, 32))))
 
     return attr.detach().numpy()
 
 
-def kshap_image_attributions(model, data, labelsToExplain):
-    pass
+def kshap_image_attributions(net, data, labelsToExplain):
+    """ Compute Kernel SHAP attribution coefficients for PyTorch model trained with image data.
+    https://captum.ai/api/kernel_shap.html
+    :param net: (object) PyTorch trained model.
+    :param data: (Tensor) tabular data observations to get the attributions for.
+    :param labelsToExplain: (Tensor) class labels the attributions will be computed w.r.t.
+    :return: (ndarray) observation feature attributions
+    """
+    shapAttr = KernelShap(net)
+
+    if len(data.shape) != 4:
+        raise ValueError('Data should be 4D (nObs x channels x height x width).')
+
+    attr = torch.FloatTensor([])
+    for obs, label in zip(data, labelsToExplain):
+        attr = torch.cat((attr, shapAttr.attribute(obs.reshape(1, 3, 32, 32), baselines=torch.zeros(data[0].shape),
+                                                   target=label)))
+
+    return attr.detach().numpy()
 
 
 def torch_pixel_attributions(model, data, labelsToExplain, method='lime', randomState=888):
@@ -42,6 +149,11 @@ def torch_pixel_attributions(model, data, labelsToExplain, method='lime', random
     """
     # todo normalize
     torch.manual_seed(randomState)
+
+    explainer = get_image_explainer(model, method=method)
+    att = get_image_attributions(explainer, inputImg=data[0].reshape(1, 3, 32, 32), target=labelsToExplain[0],
+                                 method=method)
+
     if method == 'lime':
         return lime_image_attributions(model, data, labelsToExplain)
     elif method == 'shap':
