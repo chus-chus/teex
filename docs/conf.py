3f5a4fb716b36c6797f376ffb03d9d6c46ee5d82
chus-chus <jemanac1998@gmail.com>
2022-10-17 12:11:14 +0200

    build: v1.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, Chus Antonanzas'
 author = 'Chus Antonanzas'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.0'
+release = '1.1.1'
 
 
 # -- General configuration ---------------------------------------------------
